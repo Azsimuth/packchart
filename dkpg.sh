dae821765c41781f9072e9605aa149e61587064a
Azsimuth <88154165+Azsimuth@users.noreply.github.com>
2026-03-20 22:26:42 +0100

    Delete dkpg.sh

--- dkpg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dpkg-query -Wf '${db:Status-Status} ${Installed-Size}\t${Package}\n' | sed -ne 's/^installed //p'|sort -n | cut -f1 > dkpg_sizes.txt
-dpkg-query -Wf '${db:Status-Status} ${Installed-Size}\t${Package}\n' | sed -ne 's/^installed //p'|sort -n | cut -f2 > dkpg_names.txt
